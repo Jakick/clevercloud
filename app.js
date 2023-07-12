6ef1586d4f851a93e4189a99673c53d83a6f83b7
root <qbcanes@gmail.com>
2023-07-12 06:12:31 -0400

    update

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const http = require('https');
 //const hostname = 'app-4ff09191-a8ff-4dbc-84d0-f8dcb982d186.cleverapps.io';
 const hostname = 'localhost';
 const port = 80;
+console.log('test');
+//const server = http.createServer((req, res) => {
+//	  res.statusCode = 200;
+//	  res.setHeader('Content-Type', 'text/plain');
+//	  res.end('Hello World');
+//});
 
-const server = http.createServer((req, res) => {
-	  res.statusCode = 200;
-	  res.setHeader('Content-Type', 'text/plain');
-	  res.end('Hello World');
-});
-
-server.listen(port, hostname, () => {
-	  console.log(`Server running at http://${hostname}:${port}/`);
-});
+//server.listen(port, hostname, () => {
+//	  console.log(`Server running at http://${hostname}:${port}/`);
+//});
